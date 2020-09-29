dffaaec57c55eda44c18a5e2cb3b9b3f7bf77656
damoski2 <62991627+damoski2@users.noreply.github.com>
2020-09-29 02:39:01 +0100

    service page update

--- RAMPIONTECH/App.js
+++ RAMPIONTECH/App.js
@@ -1,5 +1,22 @@
-
 //Animation on scroll
 AOS.init({
     duration:1200
 });
+
+//Project images
+
+const span_details = document.querySelectorAll('.row a');
+console.log(span_details);
+
+span_details.forEach((item)=>{
+    let spanText = item.querySelector('span');
+    item.addEventListener('mouseover',()=>{
+        spanText.style.display="block";
+    })
+
+    item.addEventListener('mouseout',()=>{
+            spanText.style.display = "none";
+    })
+})
+
+
