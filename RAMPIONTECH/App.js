9999e3c20d4abcf9055b613c955e83db3d5f78a4
Oyindacodes <akogunoyindamola42@gmail.com>
2020-09-30 16:50:15 +0100

    client_proj

--- RAMPIONTECH/App.js
+++ RAMPIONTECH/App.js
@@ -19,4 +19,31 @@ span_details.forEach((item)=>{
     })
 })
 
+/*Number countdown*/
+var client_no =100;
+var proj_no = 420;
+var counter = 10 , intervalID1, intervalID2;
 
+window.onload = calcClient();
+
+window.onload = calcProject();
+
+
+
+function calcClient(){
+  intervalID1 =  setInterval(() => {
+        var client_value = document.getElementById("client_no").innerHTML;
+        var res = Number(client_value)+counter;
+        document.getElementById("client_no").innerHTML = res;
+        Number(document.getElementById("client_no").innerHTML)==client_no? clearInterval(intervalID1) : null
+    }, 100); 
+}
+
+function calcProject(){
+    intervalID2 = setInterval(()=>{
+        var proj_value = document.getElementById("proj_no").innerHTML;
+        var res = Number(proj_value) + counter;
+        document.getElementById("proj_no").innerHTML = res;
+        Number(document.getElementById("proj_no").innerHTML)==proj_no? clearInterval(intervalID2): null
+    },100);
+}
